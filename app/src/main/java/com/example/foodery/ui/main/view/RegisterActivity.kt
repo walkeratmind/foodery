e14b1a022baa9aca1a5c22c49784f9864eaa781e
Rakesh Niraula <rakeshhniraulaa@gmail.com>
2020-07-08 15:32:24 +0545

    add login activity

---
+++ app/src/main/java/com/example/foodery/ui/main/view/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.example.foodery.ui.main.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.foodery.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        loginTextView.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        })
+
+    }
+}
